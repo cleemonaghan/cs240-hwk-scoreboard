ad579e8c6902065a4589b652eab014caca4f905a
Colin Monaghan <cleemonaghan@gmail.com>
2021-10-11 20:59:46 -0700

    Created GamePoints matrix for point storage and implemented point incrementation in the matrix in addPoint() in app.js

--- app.js
+++ app.js
@@ -3,7 +3,31 @@
  * @author Colin Monaghan
  */
 
-function addPoint() {}
+// GamePoints[0] is the home team
+// GamePoints[1] is the away team
+var GamePoints = [
+	[0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0],
+];
+
+function addPoint() {
+	//retrieve the team and inning from html
+	let team = document.querySelector("#who").value;
+	let inning = document.querySelector("#inning").value;
+
+	let gameIndex = 1;
+	if (team == "home") {
+		//increment the points for home
+		gameIndex = 0;
+	}
+	//otherwise we increment points for away
+
+	//add a point to the inning and refresh the html
+	GamePoints[gameIndex][inning - 1] += 1;
+
+	//add a point to the total and refresh the html
+	GamePoints[gameIndex][GamePoints[gameIndex].length] += 1;
+}
 
 function subPoint() {}
 
